87dd6daecf82ddb217e210f38a57548de5990241
Tsanta <tsantarakotoarisoa620@gmail.com>
2025-11-29 12:21:30 +0100

    sprint6-ter

--- src/main/java/util/http/ResponseHandler.java
+++ src/main/java/util/http/ResponseHandler.java
@@ -4,8 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
 import java.util.Map;
 
+import org.apache.commons.beanutils.ConvertUtils;
+
+import annotations.Param;
+import annotations.PathVariable;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
@@ -48,13 +54,14 @@ public class ResponseHandler {
             Method m = cm.method;
             m.setAccessible(true);
 
+            Object[] args = getMatchedParams(m, req);
             Class<?> returnType = m.getReturnType();
             Object objectController = c.getDeclaredConstructor().newInstance();
             if(returnType.equals(String.class)) {
                 res.setContentType("text/plain");
-                responseBody = m.invoke(objectController).toString();
+                responseBody = m.invoke(objectController, args).toString();
             } else if(returnType.equals(ModelAndView.class)){
-                ModelAndView mv = (ModelAndView)m.invoke(objectController);
+                ModelAndView mv = (ModelAndView)m.invoke(objectController, args);
                 Map<String, Object> data = mv.getData();
                 if(data != null) {
                     for (Map.Entry<String, Object> entry : data.entrySet()) {
@@ -73,6 +80,85 @@ public class ResponseHandler {
         } catch (ServletException | IOException ex) { // From requestDispatcher.forward()
             handleError(res, "Error forwarding to view: " + ex.getMessage());
         }
+    }
+
+private Object[] getMatchedParams(Method method, HttpServletRequest req) {
+        Parameter[] parameters = method.getParameters();
+        Object[] args = new Object[parameters.length];
+
+        Map<String, String[]> paramsViaVue = req.getParameterMap();
+        paramsViaVue.forEach((key, value) ->
+            System.out.println(key + " => " + Arrays.toString(value))
+        );
+
+        Map<String, String> pathVariables = Map.of();
+        String pathTemplate = (String) req.getAttribute("matchedRoute");
+        if(pathTemplate != null) {
+            String actualPath = req.getRequestURI().substring(req.getContextPath().length());
+            pathVariables = extractPathVariables(pathTemplate, actualPath);
+            pathVariables.forEach((key, value) ->
+                System.out.println("PATH VARIABLE: " + key + " => " + value)
+            );
+        }
+        
+        for (int i = 0; i < parameters.length; i++) {
+            Parameter p = parameters[i];
+            // @pathVarialble
+            PathVariable pathVariable = p.getAnnotation(PathVariable.class);
+            if(pathVariable != null) {
+                String varName = pathVariable.value().isEmpty() ? p.getName() : pathVariable.value();
+                if(pathVariables.containsKey(varName)) {
+                    String value = pathVariables.get(varName);
+                    Class<?> typeArg = p.getType();
+                    Object convertedValue = ConvertUtils.convert(value, typeArg);
+                    args[i] = convertedValue;
+                } else {
+                    args[i] = null;
+                }
+                continue;
+            }
+            // @param
+            String paramName;
+            Param annotation = p.getAnnotation(Param.class);
+            if(annotation != null) {
+                paramName = annotation.value();
+            } else {
+                paramName = p.getName();
+            }
+            System.out.println("PARAMNAME---- "+paramName);
+
+            if(paramsViaVue.containsKey(paramName)) {
+                String value = paramsViaVue.get(paramName)[0];
+                Class<?> typeArg = p.getType();
+
+                Object convertedValue = ConvertUtils.convert(value, typeArg);
+                args[i] = convertedValue;
+            } else {
+                args[i] = null;
+            }
+        }
+        return args;
+    }
+
+    private Map<String, String> extractPathVariables(String template, String path) {
+        String[] tSeg = template.split("/");
+        String[] pSeg = path.split("/");
+
+        if(tSeg.length != pSeg.length) return Map.of();
+
+        java.util.Map<String, String> map = new java.util.HashMap<>();
+        for (int i = 0; i < tSeg.length; i++) {
+            String ts = tSeg[i];
+            String ps = pSeg[i];
+            if(ts.startsWith("{") && ts.endsWith("}")) {
+                String name = ts.substring(1, ts.length()-1);
+                map.put(name, ps);
+            } else if(!ts.equals(ps)) {
+                // mismatch, return empty
+                return Map.of();
+            }
+        }
+        return map;
     }
 
     private void handleError(HttpServletResponse res, String error) {
