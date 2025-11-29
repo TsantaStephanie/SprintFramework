b2c091038119d8f69c9fc96f405b2631deb4a254
Tsanta <tsantarakotoarisoa620@gmail.com>
2025-11-29 11:06:13 +0100

    sprint 5

--- framework/servlet/FrontServlet.java
+++ framework/servlet/FrontServlet.java
@@ -3,56 +3,80 @@ package framework.servlet;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.io.IOException;
-import framework.model.ModelView; 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import framework.annotation.AnnotationReader;
+import framework.model.ModelView;
+import framework.utilitaire.MappingInfo;
 
 public class FrontServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) 
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String originalURI = (String) req.getAttribute("originalURI");
         String urlPath = originalURI != null ? originalURI : req.getRequestURI();
-    
-        System.out.println("Vous essayez d'acceder a : " + urlPath);
-        
-      
-        Object result = processRequest(urlPath, req, resp);
-        
-      
+
+        String contextPath = req.getContextPath();
+        String relativeUrl = urlPath.startsWith(contextPath)
+                ? urlPath.substring(contextPath.length())
+                : urlPath;
+
+        System.out.println("Vous essayez d'acceder a : " + relativeUrl);
+
+        Object result = dispatchToController(relativeUrl, req, resp);
+
         handleResult(result, req, resp);
     }
-    
+
     /**
-     * Fonction qui traite la requête et retourne soit un String soit un ModelView
+     * Trouve le mapping correspondant à l'URL et invoque la méthode du controller.
      */
-    private Object processRequest(String urlPath, HttpServletRequest req, HttpServletResponse resp) {
-        if (urlPath.contains("/api/")) {
-            return "Données API pour : " + urlPath;
-        } else {
-            ModelView mv = new ModelView();
-            mv.setView("/testFramework/index.html"); 
-            return mv;
+    private Object dispatchToController(String urlPath, HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException {
+
+        MappingInfo mappingInfo = AnnotationReader.findMappingByUrl(urlPath);
+
+        if (!mappingInfo.isFound()) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            throw new ServletException("Aucun mapping trouvé pour l'URL : " + urlPath);
+        }
+
+        try {
+            Class<?> controllerClass = mappingInfo.getControllerClass();
+            Object controllerInstance = controllerClass.getDeclaredConstructor().newInstance();
+            Method method = mappingInfo.getMethod();
+
+            // Ici on suppose que la méthode prend HttpServletRequest en paramètre
+            return method.invoke(controllerInstance, req);
+
+        } catch (InstantiationException | IllegalAccessException |
+                 NoSuchMethodException | InvocationTargetException e) {
+            throw new ServletException("Erreur lors de l'invocation du controller pour l'URL : " + urlPath, e);
         }
     }
-    
+
     /**
      * Fonction qui vérifie le type de retour et agit en conséquence
      */
-    private void handleResult(Object result, HttpServletRequest req, HttpServletResponse resp) 
+    private void handleResult(Object result, HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        
+
         if (result instanceof String) {
-            resp.setContentType("text/html");
-            resp.getWriter().write("<h1>" + result.toString() + "</h1>");
-            
-        } else if (result instanceof ModelView) {
-            
-            ModelView mv = (ModelView) result;
-            String viewPath = mv.getView();
-            
+            // String = chemin de la vue
+            String viewPath = (String) result;
             RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
             dispatcher.forward(req, resp);
-            
+
+        } else if (result instanceof ModelView) {
+
+            ModelView mv = (ModelView) result;
+            String viewPath = mv.getView();
+
+            RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+            dispatcher.forward(req, resp);
+
         } else {
             resp.setContentType("text/html");
             resp.getWriter().write("<h1>Type de retour non géré</h1>");
